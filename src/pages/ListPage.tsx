2c171e223bc498d3443b25060b6c00f772bbb32b
Arthur Bernardo Messias <turmessias@gmail.com>
2022-05-23 18:44:25 -0300

    REFACTOR - Convert ListPage page  file to typescript

---
+++ src/pages/ListPage.tsx
@@ -0,0 +1,79 @@
+import { useState } from 'react';
+import { AllUsersList } from '../components/AllUsersList';
+import { ModalEdit } from '../components/ModalEdit';
+
+export function ListPage() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [userInfo, setUserInfo] = useState(undefined);
+
+  const handleClose = () => setIsModalOpen(false);
+
+  return (
+    <>
+      <ModalEdit
+        isOpen={isModalOpen}
+        onClose={handleClose}
+        userInfo={userInfo}
+        setIsModalOpen={setIsModalOpen}
+      />
+      <AllUsersList
+        onEdit={(user) => {
+          setIsModalOpen(true);
+          setUserInfo(user as any);
+        }}
+      />
+    </>
+  );
+}
+// import { useState } from 'react';
+// import AllUsersList from '../components/AllUsersList';
+// import { ModalEdit } from '../components/ModalEdit';
+// import { useFetchUsersTable } from '../hooks/useFetchUsersTable';
+
+// export function ListPage() {
+//   const [isModalOpen, setIsModalOpen] = useState(false);
+//   const [userInfo, setUserInfo] = useState(undefined);
+//   const {
+//     usersList,
+//     setUsersList,
+//     deleteUser,
+//     isUserListUpdated,
+//     setIsUserListUpdated,
+//     updatedUsersList,
+//     allColumns,
+//     setAllColumns,
+//     standard,
+//     setStandard,
+//     filterColumn,
+//   } = useFetchUsersTable();
+
+//   const handleClose = () => setIsModalOpen(false);
+
+//   return (
+//     <>
+//       <ModalEdit
+//         isOpen={isModalOpen}
+//         onClose={handleClose}
+//         userInfo={userInfo}
+//         setIsModalOpen={setIsModalOpen}
+//         setIsUserListUpdated={setIsUserListUpdated}
+//       />
+//       <AllUsersList
+//         onEdit={(user) => {
+//           setIsModalOpen(true);
+//           setUserInfo(user);
+//         }}
+//         usersList={usersList}
+//         setUsersList={setUsersList}
+//         deleteUser={deleteUser}
+//         isUserListUpdated={isUserListUpdated}
+//         updatedUsersList={updatedUsersList}
+//         allColumns={allColumns}
+//         setAllColumns={setAllColumns}
+//         standard={standard}
+//         setStandard={setStandard}
+//         filterColumn={filterColumn}
+//       />
+//     </>
+//   );
+// }
