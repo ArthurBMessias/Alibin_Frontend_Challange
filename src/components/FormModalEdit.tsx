98342e0a15e87949d547039c0133f853994ea5fa
Arthur Bernardo Messias <turmessias@gmail.com>
2022-05-23 18:38:44 -0300

    REFACTOR - Convert FormModalEdit component  file to typescript

---
+++ src/components/FormModalEdit.tsx
@@ -0,0 +1,144 @@
+import { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { useFetchUsersTable } from '../hooks/useFetchUsersTable';
+import IFormModalProps from '../Interfaces/IFormModalProps';
+
+export function FormModalEdit({ userInfo, setIsModalOpen }: IFormModalProps) {
+  const [name, setName] = useState(userInfo?.name || '');
+  const [email, setEmail] = useState(userInfo?.email || '');
+  const [companyName, setCompanyName] = useState(userInfo?.company.name || '');
+  const [website, setWebSite] = useState(userInfo?.website || '');
+  const { updateUser } = useFetchUsersTable();
+
+  function handleEditUserInfo() {
+    const editedUser = {
+      id: userInfo.id,
+      name,
+      email,
+      companyName,
+      website,
+    };
+    updateUser(editedUser);
+  }
+
+  return (
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>USUÁRIO</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Editar Usuário"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+        <Form.Label>EMAIL</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Editar email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <Form.Label>CLIENTE</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Editar Cliente"
+          value={companyName}
+          onChange={(event) => setCompanyName(event.target.value)}
+        />
+        <Form.Label>PERFIL DE ACESSO</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Editar Perfil de Acesso"
+          value={website}
+          onChange={(event) => setWebSite(event.target.value)}
+        />
+      </Form.Group>
+      <div className="d-grid gap-2" style={{ padding: '15px' }}>
+        <Button
+          onClick={() => {
+            handleEditUserInfo();
+            setIsModalOpen(false);
+          }}
+        >
+          Salvar Alterações
+        </Button>
+      </div>
+    </Form>
+  );
+}
+
+// import { useState } from 'react';
+// import { Button, Form } from 'react-bootstrap';
+// import { useFetchUsersTable } from '../hooks/useFetchUsersTable';
+
+// export function FormModalEdit({ userInfo, setIsModalOpen, setIsUserListUpdated }) {
+//   const [name, setName] = useState(userInfo?.name || '');
+//   const [email, setEmail] = useState(userInfo?.email || '');
+//   const [companyName, setCompanyName] = useState(userInfo?.company.name || '');
+//   const [website, setWebSite] = useState(userInfo?.website || '');
+//   const { usersList, setUsersList, updateUser } = useFetchUsersTable();
+
+//   function handleEditUserInfo() {
+//     const editedUser = {
+//       id: userInfo.id,
+//       name,
+//       email,
+//       companyName,
+//       website
+//     }
+//     updateUser(editedUser);
+//   }
+
+//   return (
+//     <Form>
+//       <Form.Group className="mb-3" controlId="formBasicEmail">
+//         <Form.Label>
+//           USUÁRIO
+//         </Form.Label>
+//         <Form.Control
+//           type="text"
+//           placeholder="Editar Usuário"
+//           value={ name }
+//           onChange={ (event) => setName(event.target.value)}
+//         />
+//         <Form.Label>
+//           EMAIL
+//         </Form.Label>
+//         <Form.Control
+//           type="text"
+//           placeholder="Editar email"
+//           value={ email }
+//           onChange={(event) => setEmail(event.target.value)}
+//         />
+//         <Form.Label>
+//           CLIENTE
+//         </Form.Label>
+//         <Form.Control
+//           type="text"
+//           placeholder="Editar Cliente"
+//           value={ companyName }
+//           onChange={(event) => setCompanyName(event.target.value)}
+//         />
+//         <Form.Label>
+//           PERFIL DE ACESSO
+//         </Form.Label>
+//         <Form.Control
+//           type="text"
+//           placeholder="Editar Perfil de Acesso"
+//           value={ website }
+//           onChange={(event) => setWebSite(event.target.value)}
+//         />
+//       </Form.Group>
+//       <div className="d-grid gap-2" style={{ padding: '15px'}}>
+//         <Button
+//         onClick={ () => {
+//           handleEditUserInfo()
+//           setIsModalOpen(false)
+//           setIsUserListUpdated(true)
+//         }}>
+//           Salvar Alterações
+//         </Button>
+//       </div>
+//     </Form>
+//   );
+// }
