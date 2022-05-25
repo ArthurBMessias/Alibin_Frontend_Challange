cb3a9a48edcfdd41cd617c291ed445349de3ba90
Arthur Bernardo Messias <turmessias@gmail.com>
2022-05-25 19:02:25 -0300

    FIX - fixed indentation on DropDownConditions

--- src/components/DropDownConditions.tsx
+++ src/components/DropDownConditions.tsx
@@ -3,7 +3,8 @@ import { BiDotsVerticalRounded } from 'react-icons/bi';
 import { useFetchUsersTable } from '../hooks/useFetchUsersTable';
 
 export function DropDownConditions() {
-  const { usersList, setUsersList, allCollumns, setAllCollumns, filterColumn } = useFetchUsersTable();
+  const { usersList, setUsersList, allCollumns, setAllCollumns, filterColumn } =
+    useFetchUsersTable();
   return (
     <Dropdown>
       <Dropdown.Toggle>
@@ -12,15 +13,15 @@ export function DropDownConditions() {
       <Dropdown.Menu>
         <>Linhas por página</>
         <Form.Check type="checkbox" label="Padrão" checked={true} />
-        <Form.Check type="checkbox" label="5 Linhas" defaultChecked={false}
+        <Form.Check
+          type="checkbox"
+          label="5 Linhas"
+          defaultChecked={false}
           onChange={(e) => {
             const isChecked = e.target.checked;
-            setUsersList(() =>
-              isChecked
-                ? usersList.slice(5)
-                : usersList
-            );
-          }} />
+            setUsersList(() => (isChecked ? usersList.slice(5) : usersList));
+          }}
+        />
         <>Colunas</>
         <Form.Check
           type="checkbox"
